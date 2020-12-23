0d24fe3fbc9df28f8e46582e065839b2acf896be
Elogeek <68891109+Elogeek@users.noreply.github.com>
2020-12-23 11:43:05 +0100

    test de menu en carrousel

---
+++ script.js
@@ -0,0 +1,24 @@
+let button1 = document.getElementById('next');
+let button2 = document.getElementById('previous');
+let image1 = document.querySelector('#img1');
+let image2 = document.getElementById("img2");
+let image3 = document.getElementById("img3");
+let image4 = document.getElementById("img4");
+let image5 = document.getElementById("img5");
+
+button1.addEventListener('click', function (e) {
+     image1.style.width = 400 + "px";
+     image2.style.width = 400 + "px";
+     image3.style.width = 400 + "px";
+     image4.style.width = 400 + "px";
+     image5.style.width = 400 + "px";
+});
+
+button2.addEventListener('click', function (e) {
+    image1.style.width = 50 + "px";
+    image2.style.width = 50 + "px";
+    image3.style.width = 50 + "px";
+    image4.style.width = 50 + "px";
+    image5.style.width = 50 + "px";
+
+})
